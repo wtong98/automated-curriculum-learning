7ef2b974fc35fb7bda985d8b2fd4d0b2db8163aa
William Tong <williamtong105@gmail.com>
2023-09-24 15:20:38 -0400

    Checkpoint changes

--- trail_env/testbench/meander_estimate_qs.py
+++ trail_env/testbench/meander_estimate_qs.py
@@ -86,6 +86,73 @@ if __name__ == '__main__':
                     res[save_idx, i] = succ
 
             np.save(f'meander_{name}_probs.{model_dir.name}.npy', res)
+# <codecell>
+def plot_estimates(df, probs, name, save_path=None):
+    idx = None
+    if name == 'adp':
+        idx = 0
+    elif name == 'inc':
+        idx = 1
+    else:
+        raise ValueError('name should be "adp" or "inc"')
+
+    traj = np.array([t for t in df.runs[idx][0]])
+
+    plt.gcf().set_size_inches(8, 3)
+    ax = plt.gca()
+
+    ticks = np.arange(6)
+    ax.set_yticks(ticks, np.flip(np.arange(6) + 1))
+    ax.set_ylabel('N')
+    ax.set_xlabel('Steps')
+
+    ratio = probs[:,1:] / (probs[:,:-1] + 1e-8)
+    ratio = np.concatenate((probs[:,[0]], ratio), axis=1)
+    ratio = np.clip(ratio, 0.01, 0.99)
+    qr = np.log(ratio / (1 - ratio))
+
+    qr = probs
+    qr = np.flip(qr.T, axis=0)
+    ax.imshow(qr, aspect='auto')
+
+    ax.plot(4.6 - traj, color='red', linewidth=3)
+
+    plt.gcf().tight_layout()
+
+    if save_path is not None:
+        plt.savefig(save_path)
+
+
+estm_dir = Path('remote/trail_sample/estimates')
+estimates = {}
+
+for fp in estm_dir.iterdir():
+    if fp.suffix != '.npy':
+        continue
+
+    name, idx, _ = fp.name.split('.')
+    _, teacher_name, _ = name.split('_')
+    if idx not in estimates:
+        estimates[idx] = {}
+
+    estimates[idx][teacher_name] = np.load(fp)
+
+for fp in estm_dir.iterdir():
+    if fp.suffix != '.pkl':
+        continue
+    
+    _, _, idx = fp.stem.split('_')
+    estimates[idx]['df'] = pd.read_pickle(fp)
+
+save_dir = Path('remote/trail_sample/plots')
+if not save_dir.exists():
+    save_dir.mkdir()
+
+for idx, res in estimates.items():
+    print(res.keys())
+    for name in ('inc', 'adp'):
+        plot_estimates(res['df'], res[name], name, save_path=save_dir / f'meander_{idx}_{name}.png')
+        plt.clf()
     
 # <codecell>
 df = pd.read_pickle('remote/trail_sample/meander_results.pkl')
@@ -113,5 +180,5 @@ im = ax.imshow(qr, aspect='auto')
 ax.plot(4.6 - traj, color='red', linewidth=3)
 
 plt.gcf().tight_layout()
-plt.savefig('fig/trail_inc_probs.png')
+# plt.savefig('fig/trail_inc_probs.png')
 # %%
